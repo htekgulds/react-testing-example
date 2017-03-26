9c2e79d6f0057dbf840614fe17584d5f825c36e5
Hasan TEKGÜL <htekgulds@gmail.com>
2017-03-26 23:21:16 +0300

    ilk commit: webpack, react, standard.js ayarları yapıldı

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {HotModuleReplacementPlugin, NamedModulesPlugin} = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/main.js'
+    ],
+    output: {
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'standard-loader',
+                exclude: /node_modules/,
+                options: {
+                    error: false,
+                    snazzy: true,
+                    parser: 'babel-eslint'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'react-hot-loader'},
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [ 'latest', {'modules': false}], 'react']
+                        }}
+                ]
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: resolve(__dirname, 'dist'),
+        hot: true
+    },
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
